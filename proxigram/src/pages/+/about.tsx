f18225f8015241d3eec5ee4d199d9b01a62f371b
John Corser <johnpc@umich.edu>
2023-11-27 21:35:14 -0500

    add proxigram

---
+++ proxigram/src/pages/+/about.tsx
@@ -0,0 +1,15 @@
+import { Layout } from "@/components/layouts/Layout";
+
+export default function About() {
+	return (
+		<Layout
+			meta={{
+				title: "About",
+				description:
+					"What is Proxigram? Learn more about Proxigram. The alternative front-end for Instagram",
+			}}
+		>
+			<h2>Page under construction</h2>
+		</Layout>
+	);
+}
